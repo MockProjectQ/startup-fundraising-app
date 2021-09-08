944494637334a123979a839dd6774a7c396d038b
Harshita-Nangia <hn.nangia98@gmail.com>
2021-09-08 20:24:10 +0530

    Home Commit

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
-export default function Home() {
+import '../../App.css'
+import Cards from '../home-sections/Cards'
+import Footer from '../home-sections/Footer'
+import HeroSection from '../home-sections/HeroSection'
+import Navbar from '../navbar/Navbar'
+
+const Home = () => {
     return (
-        <div>
-            This is home Component
-        </div>
+        <>
+        <Navbar/>
+        <HeroSection />
+        <Cards/>
+        <Footer/>
+        
+      </>
     )
 }
+
+export default Home
